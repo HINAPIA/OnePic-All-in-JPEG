e5ba7672e9f64057d8e98eefc949739cfcd134cb
kyum-q <109158497+kyum-q@users.noreply.github.com>
2023-03-30 00:35:55 +0900

    [ADD] Edit Fragment set bottombar

---
+++ OnePIC/app/src/main/java/com/example/onepic/EditFragment.kt
@@ -0,0 +1,12 @@
+package com.example.onepic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class EditFragment : Fragment(R.layout.fragment_edit) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
--- OnePIC/app/src/main/java/com/example/onepic/MainActivity.kt
+++ OnePIC/app/src/main/java/com/example/onepic/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.onepic
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        closeBottomBar()
+    }
+    private fun closeBottomBar() {
+        // 전체 화면 모드로 설정
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                )
+
+        // 하단 네비게이션 바 숨기기
+        window.decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+            if ((visibility and View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0) {
+                window.decorView.systemUiVisibility = (
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        )
+            }
+        }
     }
 }
