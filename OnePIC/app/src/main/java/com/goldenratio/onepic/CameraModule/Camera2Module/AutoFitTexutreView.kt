214bb87cdd5c2a2a7bf90095b79c41ed666c810d
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-10-13 18:03:34 +0900

    [Update] 오픈소스 라이선스 기재 추가

--- OnePIC/app/src/main/java/com/goldenratio/onepic/CameraModule/Camera2Module/AutoFitTexutreView.kt
+++ OnePIC/app/src/main/java/com/goldenratio/onepic/CameraModule/Camera2Module/AutoFitTexutreView.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.test_camera2.CameraHelper
 
 import android.content.Context
@@ -6,15 +22,26 @@ import android.util.Log
 import android.view.TextureView
 import android.view.View
 
+/**
+ * A [TextureView] that can be adjusted to a specified aspect ratio.
+ */
 class AutoFitTexutreView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyle: Int = 0
+    defStyle: Int  = 0
 ) : TextureView(context, attrs, defStyle) {
 
     private var ratioWidth = 0
     private var ratioHeight = 0
 
+    /**
+     * Sets the aspect ratio for this view. The size of the view will be measured based on the ratio
+     * calculated from the parameters. Note that the actual sizes of parameters don't matter, that
+     * is, calling setAspectRatio(2, 3) and setAspectRatio(4, 6) make the same result.
+     *
+     * @param width  Relative horizontal size
+     * @param height Relative vertical size
+     */
     fun setAspectRatio(width : Int, height: Int){
         if(width < 0 || height < 0){
             throw IllegalArgumentException("Size cannot be negative.")
@@ -23,7 +50,6 @@ class AutoFitTexutreView @JvmOverloads constructor(
         ratioHeight = height
         Log.v("CameraFragment", "[setAspectRatio] AutoFitTextureView ratio width x height = ${ratioWidth} x ${ratioHeight}")
         requestLayout()
-        Thread.sleep(500)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
